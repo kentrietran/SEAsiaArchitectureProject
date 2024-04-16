7c29ba78711b88c4b84c2319e1e6eca8acd05f3f
nuclear_pasta02 <malini2700@gmail.com>
2024-04-16 16:34:47 -0400

    home2 images
    home2 images

--- server.py
+++ server.py
@@ -3,6 +3,65 @@ from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
 
+home_images ={
+
+}
+
+lessons = {
+"1":{
+    "lesson_id": 1,
+    "title": "Top 3 famous Architectural Styles in Thailand",
+    "card_image": "images/dragon_roof.png",
+    "card_text_title": ["Roof Finials", "Roof Tiers", "Indented Corners"],
+    "card_text": ["Roof finials, also known as chofa, are intricate decorative elements commonly found in Thai architecture.", "...", ".."],
+    "texts": ["These ornate structures adorn the apex of temple roofs and palace buildings, adding a touch of elegance and cultural significance to the skyline. Crafted with meticulous detail, roof finials often depict mythical creatures such as dragons or nāgas (half-human and half-serpent), symbolizing protection and prosperity in Thai culture. Their presence not only enhances the aesthetic appeal of the architecture but also serves as a reminder of Thailand's rich history and spiritual heritage.", "...", "..."],
+    "images": ["images/dragon_roof.png", "images/nagas_roof.png"],
+    "image_captions": ["Dragon roof finial", "Nagas roof finial"],
+},
+"2":{
+    "lesson_id": 2,
+    "title": "Top 3 famous Architectural Styles in Thailand",
+    "card_image": "images/dragon_roof.png",
+    "card_text_title": ["Roof Finials", "Roof Tiers", "Indented Corners"],
+    "card_text": ["Roof finials, also known as chofa, are intricate decorative elements commonly found in Thai architecture.", "...", ".."],
+    "texts": ["These ornate structures adorn the apex of temple roofs and palace buildings, adding a touch of elegance and cultural significance to the skyline. Crafted with meticulous detail, roof finials often depict mythical creatures such as dragons or nāgas (half-human and half-serpent), symbolizing protection and prosperity in Thai culture. Their presence not only enhances the aesthetic appeal of the architecture but also serves as a reminder of Thailand's rich history and spiritual heritage.", "...", "..."],
+    "images": ["images/dragon_roof.png", "images/nagas_roof.png"],
+    "image_captions": ["Dragon roof finial", "Nagas roof finial"],
+},
+"3":{
+    "lesson_id": 3,
+    "title": "Top 3 famous Architectural Styles in Thailand",
+    "card_image": "images/dragon_roof.png",
+    "card_text_title": ["Roof Finials", "Roof Tiers", "Indented Corners"],
+    "card_text": ["Roof finials, also known as chofa, are intricate decorative elements commonly found in Thai architecture.", "...", ".."],
+    "texts": ["These ornate structures adorn the apex of temple roofs and palace buildings, adding a touch of elegance and cultural significance to the skyline. Crafted with meticulous detail, roof finials often depict mythical creatures such as dragons or nāgas (half-human and half-serpent), symbolizing protection and prosperity in Thai culture. Their presence not only enhances the aesthetic appeal of the architecture but also serves as a reminder of Thailand's rich history and spiritual heritage.", "...", "..."],
+    "images": ["images/dragon_roof.png", "images/nagas_roof.png"],
+    "image_captions": ["Dragon roof finial", "Nagas roof finial"],
+},
+"4":{
+    "lesson_id": 4,
+    "title": "Top 3 famous Architectural Styles in Thailand",
+    "card_image": "images/dragon_roof.png",
+    "card_text_title": ["Roof Finials", "Roof Tiers", "Indented Corners"],
+    "card_text": ["Roof finials, also known as chofa, are intricate decorative elements commonly found in Thai architecture.", "...", ".."],
+    "texts": ["These ornate structures adorn the apex of temple roofs and palace buildings, adding a touch of elegance and cultural significance to the skyline. Crafted with meticulous detail, roof finials often depict mythical creatures such as dragons or nāgas (half-human and half-serpent), symbolizing protection and prosperity in Thai culture. Their presence not only enhances the aesthetic appeal of the architecture but also serves as a reminder of Thailand's rich history and spiritual heritage.", "...", "..."],
+    "images": ["images/dragon_roof.png", "images/nagas_roof.png"],
+    "image_captions": ["Dragon roof finial", "Nagas roof finial"],
+},
+}
+
+quiz = {
+"1": {
+"image": "images/dragon_roof.png",
+"answer": "",
+"page": "",
+},
+"2": {
+"image": "images/dragon_roof.png",
+"answer": "",
+"page": "",
+},
+}
 
 # ROUTES
 
@@ -10,9 +69,13 @@ app = Flask(__name__)
 def home():
    return render_template('home.html')
 
+@app.route('/home2')
+def home2():
+   return render_template('home2.html')
+
 @app.route('/learn')
 def learn():
-   return render_template('learn.html')
+    return render_template('learn.html')
 
 @app.route('/quiz')
 def quiz():
